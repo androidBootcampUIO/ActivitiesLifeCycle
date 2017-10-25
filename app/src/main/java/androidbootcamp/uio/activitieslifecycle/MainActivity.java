48232d72100a8282900fa678f62b643003273008
Sebastian Puente <sebas7pr@gmail.com>
2017-10-25 15:42:25 -0500

    Muestra mensajes Toast con el nombre del metodo que esta siendo llamado en cada etapa del ciclo de vida del Activity

--- app/src/main/java/androidbootcamp/uio/activitieslifecycle/MainActivity.java
+++ app/src/main/java/androidbootcamp/uio/activitieslifecycle/MainActivity.java
@@ -2,6 +2,7 @@ package androidbootcamp.uio.activitieslifecycle;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,67 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        printMethodNameOnToast("onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        printMethodNameOnToast("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        printMethodNameOnToast("onResume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        printMethodNameOnToast("onPause");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        printMethodNameOnToast("onStop");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        printMethodNameOnToast("onRestart");
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        printMethodNameOnToast("onSaveInstanceState");
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        printMethodNameOnToast("onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        printMethodNameOnToast("onDestroy");
+
+    }
+
+    private void printMethodNameOnToast(String methodName) {
+        Toast.makeText(this.getApplicationContext(),
+                String.format("Método invocado: %s", methodName),
+                Toast.LENGTH_LONG)
+                .show();
     }
 }
